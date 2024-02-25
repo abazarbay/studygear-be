6260d00277ed8b6bcaea8e461639c5d8532de0f4
abazarbay <aigulbazarbay@gmail.com>
2024-02-25 20:34:55 +0100

    9876

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module '@babel/runtime/helpers/interopRequireDefault' {
+    export default function _interopRequireDefault(obj: any): any;
+    
+  }
+  declare module '@babel/runtime/helpers/asyncToGenerator' {
+    export default function _asyncToGenerator(fn: any): any;
+  }
+    declare module '@babel/runtime/regenerator' {
+    export default function _regeneratorRuntime(fn: any): any;
+    }
