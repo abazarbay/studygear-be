440a79a996ceae9b41fa26e47dd3374bc5b5d9c3
abazarbay <aigulbazarbay@gmail.com>
2024-02-25 21:48:24 +0100

    987654 some changes34

--- src/server.js
+++ src/server.js
@@ -14,11 +14,11 @@ app.use(history());
 app.get('/api/programms', async (req, res) => {
     const client = await MongoClient.connect(
       process.env.MONGO_USER && process.env.MONGO_PASS 
-      ? `mongodb+srv://studygear-backend:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`
       : 'mongodb://localhost:27017', 
       { }
     );
-const db = client.db('vue-db');
+const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
 const programms = await db.collection('programms').find({}).toArray();
 res.status(200).json(programms);
 client.close(); 
@@ -28,11 +28,11 @@ app.get('/api/users/:userId/cart', async (req, res) => {
   const { userId } = req.params;
   const client = await MongoClient.connect( 
     process.env.MONGO_USER && process.env.MONGO_PASS 
-    ? `mongodb+srv://studygear-backend:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`
     : 'mongodb://localhost:27017', 
      { }
   );
-  const db = client.db('vue-db');
+  const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
   const user = await db.collection('users').findOne({ id: userId });
   if (!user) return res.status(404).json({ message: 'User not found' });
   const programms = await db.collection('programms').find({}).toArray();
@@ -47,11 +47,11 @@ app.get('/api/programms/:programmId', async (req, res) => {
     const { programmId } = req.params;
     const client = await MongoClient.connect(
       process.env.MONGO_USER && process.env.MONGO_PASS 
-      ? `mongodb+srv://:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`
       : 'mongodb://localhost:27017', 
       { }
     );
-    const db = client.db('vue-db');
+    const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
     const programm = await db.collection('programms').findOne({ id: programmId });
     if (programm) {
       res.status(200).json(programm);
@@ -66,11 +66,11 @@ app.post('/api/users/:userId/cart', async (req, res) => {
     const { programmId } = req.body;
     const client = await MongoClient.connect(
       process.env.MONGO_USER && process.env.MONGO_PASS 
-      ? `mongodb+srv://studygear-backend:${process.env.MONGO_PASS}:@cluster0.wos7rvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`
       : 'mongodb://localhost:27017', 
       { }
     );
-    const db = client.db('vue-db');
+    const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
     await db.collection('users').updateOne({ id: userId }, 
       { $addToSet: { cartItems: programmId } 
     });
@@ -87,11 +87,11 @@ app.delete('/api/users/:userId/cart/:programmId', async (req, res) => {
     const { userId, programmId } = req.params;
     const client = await MongoClient.connect(
       process.env.MONGO_USER && process.env.MONGO_PASS 
-      ? `mongodb+srv://studygear-backend:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wos7rvs.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority&appName=Cluster0`
       : 'mongodb://localhost:27017', 
       { }
     );
-    const db = client.db('vue-db');
+    const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
   
     await db.collection('users').updateOne({ id: userId }, { $pull: { cartItems: programmId } });
     const user = await db.collection('users').findOne({ id: userId });
